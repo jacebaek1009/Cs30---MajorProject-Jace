11bc7b47e68569d6c8d983fff498284dacc90670
jacebaek1009 <jacebaek06@gmail.com>
2023-11-24 12:17:26 -0600

    displaying new background and displaying baskets

--- sketch.js
+++ sketch.js
@@ -5,11 +5,70 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let ingredient = [];
+let salmon;
+let basket;
+let grab = true;
+let backg;
+let egg;
+let eggBasket;
+
+function preload() {
+  salmon = loadImage("salmon.png");
+  basket = loadImage("basket.png");
+  backg = loadImage("background.png");
+  egg = loadImage("egg.png");
+  eggBasket = loadImage("eggbasket.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(backg);
+  let salmonPlace = new Ingredients(mouseX, mouseY, salmon);
+  let eggBasket = new Basket(windowWidth/2 windowHeight/2);
+  ingredient.push(salmonPlace);
 }
 
 function draw() {
-  background(220);
+  for(let i of ingredient) {
+    i.display();
+  }
+
 }
+function mousePressed() {
+  let place = new Ingredients(mouseX, mouseY, salmon);
+  ingredient.push(place);
+}
+
+
+class Ingredients {
+  constructor(x, y, type){
+    this.x = x;
+    this.y = y;
+    this.type = type;
+  }
+
+  display() {
+    noStroke();
+    fill(255, 0 , 0);
+    image(this.type, this.x, this.y);
+  }
+}
+
+
+class Basket {
+  constructor(x, y, type){
+    this.x = x;
+    this.y = y;
+    this.type = type;
+  }
+
+  display(){
+    image(this.type, this.x, this.y);
+  }
+}
+
+function isInBasket(x, y, top, bottom, left, right) {
+  return x >= left && x <=right && (y <= bottom && y >= top);
+}
+
